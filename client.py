06dedb30c04db6c1b4e6c98464403f76c53ae524
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2022-11-17 20:33:17 +0000

    Start with client code

--- client.py
+++ client.py
@@ -6,41 +6,60 @@ import selectors
 import socket
 import time
 import types
-
+ROUTER_IP='127.0.0.1'
+CLIENT_IP='127.0.0.1'
+ROUTER_PORT=33301
+CLIENT_PORT=33302
 data_dictionary = {'cpu_temperature': 'None', 'gpu_temperature': 'None'}
 def getData(data_type):
     requestData(data_type)
-    while(!data_dictionary[data_type]):
-        #
+    while data_dictionary[data_type] == None:
+        pass
     return data_dictionary[data_type]
 
 def requestData(data_type):
-    return 0
+    send(ROUTER_IP,ROUTER_PORT,'request:'+data_type)
 
 def advertiseDataType(data_type):
     return 0
 
 def processRequest(source_host, data_type, data):
-    return 0
+    data_dictionary[data_type] = data
 
-def sendData(command):
-        """Send sensor data to all peers."""
+def send(ip,port,msg):
         sent = False
-        # if command == 'ALERT':
-        hardcodedPeers = {('10.35.70.34', 33301), ('10.35.70.31', 33301)}
-        for peer in hardcodedPeers:
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(peer)
-                msg = command
-                s.send(msg.encode())
-                sent = True
-                s.close()
-            except Exception:
-                print("An exception occured")
+        # if command == 'ALERT':´
+        peer = (ip,port)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(peer)
+            s.send(msg.encode())
+            sent = True
+            s.close()
+        except Exception:
+            print("Exception")
         if sent:
-            print("Sensor data sent to known vehicles")
+            print("Transmission finished")
 
+def listen():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((CLIENT_IP, CLIENT_PORT))
+        while True:
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print('Message received from IP 1: ', addr)
+                    print('Data received is 1: ', data)
+                    new_data = data.decode('utf-8')
+                    data2 = new_data + " Jai Shree Ram"
+                    data3 = data2 + "Data appended by Amogh"
+                    data4 = send_data(data3, HOST, PORT)
+                    conn.sendall(data4)
 def main():
     # parser = argparse.ArgumentParser()
     # parser.add_argument('--command', help='Type of sensor', required=True)
