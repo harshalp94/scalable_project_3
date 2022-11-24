12098fe405f3b2b563ef311963c1c3087f5de633
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2022-11-24 16:09:54 +0000

    add send interest functionality to client

--- client.py
+++ client.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
 import base64
-import argparse
-import random
-import selectors
 import socket
 import time
-import types
+
 ROUTER_IP='127.0.0.1'
-CLIENT_IP='127.0.0.1'
 ROUTER_REQUEST_PORT=33301
-CLIENT_PORT=33302
+CLIENT_IP='127.0.0.1'
+LISTEN_PORT=33302
 data_dictionary = {'cpu_temperature': 'None', 'gpu_temperature': 'None'}
 
 
@@ -21,39 +18,34 @@ def base64encode(msg):
 def base64decode(msg):
     return base64.b64decode(msg.encode("ascii")).decode("ascii")
 
-def getData(data_type):
-    requestData(data_type)
-    while data_dictionary[data_type] == None:
-        pass
-    return data_dictionary[data_type]
 
-
-def processRequest(source_host, data_type, data):
-    data_dictionary[data_type] = data
-
-def send(ip,port,msg):
-        sent = False
-        # if command == 'ALERT':´
+def send(ip, port, msg):
         peer = (ip, port)
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect(peer)
             s.send(str(base64encode(msg)).encode())
             answer = s.recv(1024)
-            sent = True
             s.close()
+            if answer:
+                return base64decode(answer.decode('utf-8'))
+            else:
+                return None
         except Exception:
             print("Exception")
-        if sent:
-            print("Transmission finished")
-            return base64decode(answer.decode('utf-8'))
 
-def requestData(data_type):
-    return send(ROUTER_IP, ROUTER_REQUEST_PORT, data_type)
+
+def get_data(data_type):
+    result = send(ROUTER_IP, ROUTER_REQUEST_PORT, data_type)
+    if result:
+        return result
+    else:
+        return listen()
+
 
 def listen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((CLIENT_IP, CLIENT_PORT))
+        s.bind((CLIENT_IP, LISTEN_PORT))
         while True:
             s.listen()
             conn, addr = s.accept()
@@ -63,6 +55,7 @@ def listen():
                     data = conn.recv(1024)
                     if not data:
                         break
+                    return data
                     print('Message received from IP 1: ', addr)
                     print('Data received is 1: ', data)
                     new_data = data.decode('utf-8')
@@ -70,15 +63,16 @@ def listen():
                     data3 = data2 + "Data appended by Amogh"
                     data4 = send_data(data3, HOST, PORT)
                     conn.sendall(data4)
+
+
 def main():
     # parser = argparse.ArgumentParser()
     # parser.add_argument('--command', help='Type of sensor', required=True)
     # args = parser.parse_args()
     command_name = ['vehicle/speed', 'vehicle/proximity', 'vehicle/tyre/pressure']
     while True:
-        print("Enter data to fetch:")
-        data_type = input()
-        requestData(data_type)
+        data_type = input("Enter data to fetch:")
+        print(data_type + ": " + get_data(data_type))
         time.sleep(5)
     # port_table = {'VehiclePort': 33401}  # Hardcoded port table for testing
     # port = port_table['VehiclePort']     # The port used by the server
