b71daf2679ee63bd1d595adc32971e043a8cd88b
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2022-11-24 15:16:06 +0000

    add send interest functionality to client

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import base64
 import argparse
 import random
 import selectors
@@ -8,17 +9,24 @@ import time
 import types
 ROUTER_IP='127.0.0.1'
 CLIENT_IP='127.0.0.1'
-ROUTER_PORT=33301
+ROUTER_REQUEST_PORT=33301
 CLIENT_PORT=33302
 data_dictionary = {'cpu_temperature': 'None', 'gpu_temperature': 'None'}
+
+
+def base64encode(msg):
+    return base64.b64encode(msg.encode("ascii")).decode("ascii")
+
+
+def base64decode(msg):
+    return base64.b64decode(msg.encode("ascii")).decode("ascii")
+
 def getData(data_type):
     requestData(data_type)
     while data_dictionary[data_type] == None:
         pass
     return data_dictionary[data_type]
 
-def requestData(data_type):
-    send(ROUTER_IP, ROUTER_PORT, 'request:'+data_type)
 
 def processRequest(source_host, data_type, data):
     data_dictionary[data_type] = data
@@ -26,17 +34,22 @@ def processRequest(source_host, data_type, data):
 def send(ip,port,msg):
         sent = False
         # if command == 'ALERT':´
-        peer = (ip,port)
+        peer = (ip, port)
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect(peer)
-            s.send(msg.encode())
+            s.send(str(base64encode(msg)).encode())
+            answer = s.recv(1024)
             sent = True
             s.close()
         except Exception:
             print("Exception")
         if sent:
             print("Transmission finished")
+            return base64decode(answer.decode('utf-8'))
+
+def requestData(data_type):
+    return send(ROUTER_IP, ROUTER_REQUEST_PORT, data_type)
 
 def listen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -63,9 +76,9 @@ def main():
     # args = parser.parse_args()
     command_name = ['vehicle/speed', 'vehicle/proximity', 'vehicle/tyre/pressure']
     while True:
-        for c in command_name:
-            time.sleep(10)
-            sendData(c)
+        print("Enter data to fetch:")
+        data_type = input()
+        requestData(data_type)
         time.sleep(5)
     # port_table = {'VehiclePort': 33401}  # Hardcoded port table for testing
     # port = port_table['VehiclePort']     # The port used by the server
