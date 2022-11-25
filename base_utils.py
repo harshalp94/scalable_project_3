ae88de8689598491cc07f9544efa7a53c2334fbe
harshalp94 <harshalp94@gmail.com>
2022-11-25 13:11:14 +0000

    Changing the name of the functions to a more meaningful one

--- base_utils.py
+++ base_utils.py
@@ -1,13 +1,13 @@
 import base64
 
 
-def bencode(toEncode):
+def encode_msg(toEncode):
     ascii_encoded = toEncode.encode("ascii")
     base64_bytes = base64.b64encode(ascii_encoded)
     base64_string = base64_bytes.decode("ascii")
     return base64_string
 
-def bdecode(toDecode):
+def decode_msg(toDecode):
     base64_bytes = toDecode.encode("ascii")
     sample_string_bytes = base64.b64decode(base64_bytes)
     sample_string = sample_string_bytes.decode("ascii")
