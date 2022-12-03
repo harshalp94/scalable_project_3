c1c8e5521e2c0c47c59fd639778363095ea52d98
harshalp94 <harshalp94@gmail.com>
2022-12-03 13:55:57 +0000

    Adding runme.sh and pass.sh

---
+++ runme.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter username: "
+read user_name
+#echo $user_name
+
+echo "Enter Password: "
+echo "Type password: (Password Hidden)"
+read -s pass_name
+
+#current_dir = $PWD
+
+echo $PWD
+nohup python3 router.py &
+
+echo $pass_name | ./pass.sh ssh -o StrictHostKeyChecking=no $user_name@rasp-040.berry.scss.tcd.ie "nohup python3 $PWD/producer.py train 1 >/dev/null 2>&1 &"
+echo $pass_name | ./pass.sh ssh -o StrictHostKeyChecking=no $user_name@rasp-041.berry.scss.tcd.ie "nohup python3 $PWD/producer.py metro 1 >/dev/null 2>&1 &"
+echo $pass_name | ./pass.sh ssh -o StrictHostKeyChecking=no $user_name@rasp-042.berry.scss.tcd.ie "nohup python3 $PWD/producer.py bus 1   >/dev/null 2>&1 &"
+echo $pass_name | ./pass.sh ssh -o StrictHostKeyChecking=no $user_name@rasp-043.berry.scss.tcd.ie "nohup python3 $PWD/producer.py tram 1  >/dev/null 2>&1 &"
+echo $pass_name | ./pass.sh ssh -o StrictHostKeyChecking=no $user_name@rasp-044.berry.scss.tcd.ie "nohup python3 $PWD/producer.py taxi 1  >/dev/null 2>&1 &"
+
